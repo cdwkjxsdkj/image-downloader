1535b408ee9b3484c7ae73ffc2343976fc672f79
cdwkjxsdkj <153494120+cdwkjxsdkj@users.noreply.github.com>
2023-12-25 02:27:31 -0500

    Update imagedownloader.py

--- imagedownloader.py
+++ imagedownloader.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-#url = 'https://www.airbnb.co.uk/s/Ljubljana--Slovenia/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&query=Ljubljana%2C%20Slovenia&place_id=ChIJ0YaYlvUxZUcRIOw_ghz4AAQ&checkin=2020-11-01&checkout=2020-11-08&source=structured_search_input_header&search_type=autocomplete_click'
+#url = https://www.goat.com/search?query=nike+shoes&brand=nike
 
 def imagedown(url, folder):
     try:
